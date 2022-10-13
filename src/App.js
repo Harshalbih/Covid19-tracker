ffeca1dce58808cd67818d88b8257200a959bcb2
Harshal Bihade <63906732+Harshalbih@users.noreply.github.com>
2022-10-14 00:10:04 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import './App.css';
+import React from "react"
+import { useState, useEffect } from 'react';
+
+function App(){
+const [data, setData] = useState([]);
+
+useEffect (() => {
+    fetch("https://data.covid19india.org/data.json")
+    .then((res) => res.json())
+    .then((jsondata) => setData(jsondata.statewise))
+}, []);
+ return (
+   <div className='imagevirus'>
+    <center>
+    <h3> Covid 19 tracker </h3>
+    <table  className="table table-dark table-hover">
+      <thead>
+        <tr>
+          <th>State</th>
+          <th>Confirmed</th>
+          <th>Recovered</th>
+          <th>Deaths</th>
+          <th>Active</th>
+          <th>LastUpdated</th>
+        </tr>
+        </thead>
+        <tbody>
+          {data.map((item) => {
+          return(
+            <tr>
+              <td>{item.state}</td>
+              <td>{item.confirmed}</td>
+              <td>{item.recovered}</td>
+              <td>{item.deaths}</td>
+              <td>{item.active}</td>
+              <td>{item.lastupdatedtime}</td>
+            </tr>
+          )
+        })};
+          </tbody>
+          </table>   
+    </center>
+</div>
   );
-}
+ }
 
 export default App;
+
+
+
